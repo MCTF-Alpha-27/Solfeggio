de1b91af6d8ee9b6261161497699047490d1db57
MCTF-Alpha-27 <85936935+MCTF-Alpha-27@users.noreply.github.com>
2024-01-26 14:44:37 +0800

    小修小补

--- solfeggio.py
+++ solfeggio.py
@@ -6,7 +6,7 @@ from cmd import Cmd
 from typing import IO
 
 __author__ = "MCTF-Alpha-27"
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 class Solfeggio(Cmd):
     intro = "欢迎使用视唱练耳命令行\n"
@@ -20,10 +20,17 @@ class Solfeggio(Cmd):
         self.note_freqs = [262, 294, 330, 349, 392, 440, 494]
 
     def play_note(self, frequency, duration=1000):
-        if not 1 <= int(frequency) <= 7:
-            print("唱名的简谱写法无效。\n")
-            return
-        winsound.Beep(self.note_freqs[frequency - 1], duration)
+        if not frequency.isdigit():
+            note = next((key for key, value in self.number_to_name.items() if value == frequency), None)
+            if not note:
+                print("唱名名称无效。\n")
+                return
+            winsound.Beep(self.note_freqs[int(note) - 1], duration)
+        else:
+            if not 1 <= int(frequency) <= 7:
+                print("唱名的简谱写法无效。\n")
+                return
+            winsound.Beep(self.note_freqs[int(frequency) - 1], duration)
 
     def do_cls(self, *ignored):
         """
@@ -52,13 +59,19 @@ class Solfeggio(Cmd):
 
         用法：showname [note_number]
             没有参数        转换上一组视唱练耳练习的写法，如果有。
-            note_number    一个唱名的简谱写法。
+            note_number    一个唱名的简谱写法，可有多个，使用空格分隔。
         """
         if note_number:
-            if not 1 <= int(note_number) <= 7:
-                print("唱名的简谱写法无效。\n")
-                return
-            print(note_number, "->", self.number_to_name[note_number])
+            for i in note_number.split(" "):
+                if not (i.isdigit() or i.isspace()):
+                    print("唱名的简谱写法%s无效。"%i)
+                    continue
+                if i.isspace():
+                    continue
+                if not 1 <= int(i) <= 7:
+                    print("唱名的简谱写法%s无效。"%i)
+                    continue
+                print(i, "->", self.number_to_name[i])
             print()
         else:
             if len(self.groups) == 0:
@@ -71,16 +84,14 @@ class Solfeggio(Cmd):
         """
         播放一个唱名的声音。
 
-        用法：playname [note_number]
+        用法：playname [note_number/note_name]
             没有参数        播放上一组视唱练耳练习的唱名声音，如果有。
-            note_number    一个唱名的简谱写法。
+            note_number    一个唱名的简谱写法，可有多个，使用空格分隔。
+            note_name      一个唱名的名称，可有多个，使用空格分隔。
         """
         if note_number:
-            note_number = int(note_number)
-            if not 1 <= note_number <= 7:
-                print("唱名的简谱写法无效。\n")
-                return
-            self.play_note(note_number)
+            for i in note_number.split(" "):
+                self.play_note(i)
         else:
             if len(self.groups) == 0:
                 return
